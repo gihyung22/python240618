175649f98ed24a8e5cd2d5177af9061b549d6cae
gihyung22 <gihyung22@gmail.com>
2024-06-18 10:39:37 +0900

    클래스 연습 완료

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,13 +1,17 @@
+# 전역변수
 strName = "Not Class Member"
 
 class DemoString:
     def __init__(self):
+        # 멤버변수
         self.strName = "" 
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(str)
+        # 파이썬은 모호한 것보다 명시적인 것이 좋다. self.strName 인데  self 누락시에는 어떻게 될까?
+        print(strName)
 
+# 인스턴스를 생성
 d = DemoString()
 d.set("First Message")
 d.print()
