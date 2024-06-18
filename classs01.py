dd08750a64bd09dab8803be12032c56641f390f6
gihyung22 <gihyung22@gmail.com>
2024-06-18 09:43:46 +0900

    클래스 연습완료

---
+++ classs01.py
@@ -0,0 +1,10 @@
+# 클래스 정의
+class Person:
+    def __init__(self):
+        self.name="default name"
+        
+    def print(self):
+        print("My name is {0}".format(self.name))
+
+p1=Person()
+p1.print()
